7b20f3e75af60404c1e48a7f1dcc3e89b325977d
Rj <rikrdo.ortega@gmail.com>
2019-06-01 22:25:15 -0600

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |categories, value| 
+    value.each do |property, values|
+      values.each do |names|
+        pigeon_list[names] ||= {}
+        pigeon_list[names][categories] ||= []
+        pigeon_list[names][categories] << property.to_s
+      end
+    end
+  end
+  pigeon_list
 end
