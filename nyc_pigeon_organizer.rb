69666e6a2d3a09434d00ae85d0e3b21797071fb4
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-06-22 17:18:21 -0600

    Sort information from given hash; store in new hash
    Practice accessing and manipulating data stored inside nested data types

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+#generate an array of pigeons; set each pigeon as a key pointing to a value of an empty array, and add to pigeon_list hash
+  data.map {|key, value| value.map {|characteristic, pigeon| pigeon }}.flatten.uniq.map {|pigeon| pigeon_list[pigeon] = {}} 
+#populate each pigeon's empty array with the keys :color, :gender and :lives; each of these will be a key pointing to an empty array
+  data.keys.map {|key| pigeon_list.map {|pigeon, hash| hash[key] = []}} 
+#go through each possible type of characteristic in original data set, looking at each bird then push the type of characteristic to pigeon_list's corresponding array
+  data.map {|quality, types| types.map {|type, birds| birds.map {|bird| pigeon_list[bird][quality].push(type.to_s)}} }
+#return new hash
+  pigeon_list
 end
