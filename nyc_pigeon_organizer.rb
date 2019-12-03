ce0e98d4a9858a79d21816ab30990dfc51aecc24
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-12-03 13:29:19 -0700

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  pigeon_names = data.collect do |attributes, details|
+    details.values
+  end
+  .flatten.uniq
+  pigeon_names.each {|name| new_hash[name] = {}}
+  data.each do |attributes, details|
+    new_hash.each do |name, hash|
+      new_hash[name][attributes] = []
+    end
+  end
+  data.each do |attribute, details|
+    details.each do |property, names|
+      names.each do |name|
+        new_hash[name][attribute] << property.to_s
+      end
+    end
+  end
+  
+  new_hash
+
 end
